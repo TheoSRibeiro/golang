cd92cdc3bf58043e517781897403a46e0ab53095
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-14 11:43:50 -0300

    Pacotes Externos

--- 1 - pacotes/main.go
+++ 1 - pacotes/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro)
+
+	erro2 := checkmail.ValidateFormat("devbook@.com")
+	fmt.Println(erro2)
 }
