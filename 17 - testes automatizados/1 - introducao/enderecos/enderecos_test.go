c8a40f6cce721bf63fea0a0ea669c51646b6986e
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-20 08:46:06 -0300

    Teste Unitario com diversos Cenarios

--- 17 - testes automatizados/1 - introducao/enderecos/enderecos_test.go
+++ 17 - testes automatizados/1 - introducao/enderecos/enderecos_test.go
@@ -2,13 +2,36 @@ package enderecos
 
 import "testing"
 
+type cenarioTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 //TESTE UNITARIO
 func TestTipoEndereco(t *testing.T) {
-	enderecoTeste := "Rua Paulista"
+
+	cenariosTeste := []cenarioTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes ABC", "Rodovia"},
+		{"Praça das Rosas", "Tipo Inválido"},
+		{"Estrada ABC", "Estrada"},
+		{"AVENIDA ABC", "Avenida"},
+		{"", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosTeste {
+		retornoRecebido := TipoEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("O O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", retornoRecebido, cenario.retornoEsperado)
+		}
+	}
+
+	/* enderecoTeste := "Rua Paulista"
 	tipoEnderecoEsperado := "Avenida"
 	tipoEnderecoRecebido := TipoEndereco(enderecoTeste)
 
 	if tipoEnderecoRecebido != tipoEnderecoEsperado {
 		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", tipoEnderecoEsperado, tipoEnderecoRecebido)
-	}
+	} */
 }
