f5d840d83687b1d2d5f8c3e7548a464ab6c42679
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-20 15:00:58 -0300

    Rendenrizando pagina HTML

--- 20 - html/main.go
+++ 20 - html/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Olá mundo!")) //[]byte -> formato slice de byte
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
 }
 
 func main() {
 
-	http.HandleFunc("/home", home)
+	templates = template.Must(template.ParseGlob("*.html"))
 
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		//w.Write([]byte("Olá mundo!")) //[]byte -> formato slice de byte
+
+		u := usuario{"João", "joao@gmail.com"}
+		templates.ExecuteTemplate(w, "home.html", u)
+	})
+
+	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
