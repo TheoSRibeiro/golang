4aca2cfed2204c8581dbd482061aa47ca3618384
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-21 16:25:27 -0300

    Deletando dados

--- 22 - crud basico/servidor/servidor.go
+++ 22 - crud basico/servidor/servidor.go
@@ -187,4 +187,39 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// DeletarUsuario remove um usuario do BD
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 64)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar no banco de dados!"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("delete from usuarios where id = ?")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement!"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(ID); erro != nil {
+		w.Write([]byte("Erro ao remover o usuário!"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
 }
