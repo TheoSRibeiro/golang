3427e3ebc95322cfca854fbea402c59fd2b07258
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-21 10:37:34 -0300

    Estrutura para o CRUD

---
+++ 22 - crud basico/banco/banco.go
@@ -0,0 +1,22 @@
+package banco
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" //Driver de conexao com o MySQL
+)
+
+func Conectar() (*sql.DB, error) {
+	urlConexao := "golang:123456@/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", urlConexao)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	return db, nil
+}
