51dd9ef51103ad60b5f10da941d723ebb2a04ac5
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-22 10:51:33 -0300

    Criacao de usuario

--- devbook/api/src/repositorios/repositorios.go
+++ devbook/api/src/repositorios/repositorios.go
@@ -18,6 +18,26 @@ func NovoRepositorioUsuarios(db *sql.DB) *usuarios {
 //METODOS
 
 //Inserir um usuario no DB
-func (u usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	return 0, nil
+func (repositorio usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+
+	statement, erro := repositorio.db.Prepare(
+		"insert into usuarios (nome, nick, email, senha) values(?,?,?,?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+
 }
