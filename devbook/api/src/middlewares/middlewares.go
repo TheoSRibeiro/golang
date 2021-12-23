a5186ad72e6840b2f58710a1b2419cdd8dcbed71
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-23 13:53:07 -0300

    Pacote de Middlewares

---
+++ devbook/api/src/middlewares/middlewares.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+//verifica se o usr ao fazer a requisicao esta autenticado
+func Autenticar(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Autenticando...")
+		next(w, r) //se autenticar, faz a funcao da rota
+	}
+
+}
+
+//Escreve informacoes da requisicao no terminal
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
