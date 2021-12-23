c28cb779c8bd62300d2acba106abbe36a055def7
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-23 14:58:57 -0300

    Extrair o usrID do Token

--- devbook/api/src/autenticacao/token.go
+++ devbook/api/src/autenticacao/token.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -36,7 +37,7 @@ func ValidarToken(r *http.Request) error {
 		return nil
 	}
 
-	return errors.New("Token inválido!")
+	return errors.New("token inválido")
 }
 
 func extrairToken(r *http.Request) string {
@@ -46,6 +47,25 @@ func extrairToken(r *http.Request) string {
 		return strings.Split(token, " ")[1]
 	}
 	return ""
+}
+
+//Retornar o usuarioId que esta salvo no token
+func ExtrairUsuarioID(r *http.Request) (uint64, error) {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveVerificacao)
+	if erro != nil {
+		return 0, erro
+	}
+
+	if permissoes, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		usuarioID, erro := strconv.ParseUint(fmt.Sprintf("%.0f", permissoes["usuarioId"]), 10, 64)
+		if erro != nil {
+			return 0, erro
+		}
+		return usuarioID, nil
+	}
+
+	return 0, errors.New("token invalido")
 }
 
 func retornarChaveVerificacao(token *jwt.Token) (interface{}, error) {
