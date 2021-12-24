6c68cdd8939e5e4d965fe8405150202967c91d8b
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-24 10:10:47 -0300

    Rotas de Publicacoes

---
+++ devbook/api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+//Adicionar nova publicacao no DB
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Retorna as publicacoes que aparecem no feed do usuario
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Retorna 1 publicacao expecifica
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Atualiza a publicacao
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Exclui a publicacao
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
