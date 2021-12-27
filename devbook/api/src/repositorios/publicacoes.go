616bc48acf34557b1d1db3083bc31ca3ba766294
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-27 10:50:02 -0300

    Curtir Publicacao

--- devbook/api/src/repositorios/publicacoes.go
+++ devbook/api/src/repositorios/publicacoes.go
@@ -180,3 +180,22 @@ func (repositorio Publicacoes) Deletar(publicacaoId uint64) error {
 
 	return nil
 }
+
+//Adiciona uma curtida em uma publicacao
+func (repositorio Publicacoes) Curtir(publicacaoId uint64) error {
+	statement, erro := repositorio.db.Prepare(`
+	update publicacoes 
+	set curtidas = curtidas + 1
+	where id = ?`,
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoId); erro != nil {
+		return erro
+	}
+
+	return nil
+}
