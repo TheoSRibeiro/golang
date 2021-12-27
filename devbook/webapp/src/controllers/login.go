574ec1b2d62004805bd5f004d0144ef33ffa58de
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-27 13:46:38 -0300

    Templates HTML

--- devbook/webapp/src/controllers/login.go
+++ devbook/webapp/src/controllers/login.go
@@ -1,8 +1,11 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"webapp/src/utils"
+)
 
 //Carregar a tela de login
 func CarregarTelaLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("tela de login"))
+	utils.ExecutarTemplate(w, "login.html", nil)
 }
