69ecb9e47c0c48d57dc5b157095f67a82d052316
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-28 15:02:48 -0300

    Criacao de Publicacoes

---
+++ devbook/webapp/src/controllers/publicacoes.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"webapp/src/config"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
+)
+
+//Chama a API para cadastrar uma publicacao no DB
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	publicacao, erro := json.Marshal(map[string]string{
+		"titulo":   r.FormValue("titulo"),
+		"conteudo": r.FormValue("conteudo"),
+	})
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	//FAZER a REQUISICAO
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, bytes.NewReader(publicacao))
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+}
