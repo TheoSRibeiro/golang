442fb79776b8fcc86af1eba2c44b9f6bf4cf1f9b
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-28 09:48:10 -0300

    Configurando Cookies

---
+++ devbook/webapp/src/cookies/cookies.go
@@ -0,0 +1,36 @@
+package cookies
+
+import (
+	"net/http"
+	"webapp/src/config"
+
+	"github.com/gorilla/securecookie"
+)
+
+var s *securecookie.SecureCookie
+
+//Utiliza as variaveis de ambiente para criacao do SecureCookie
+func Configurar() {
+	s = securecookie.New(config.HashKey, config.BlockKey)
+}
+
+//Registra as informacoes de autenticacao
+func Salvar(w http.ResponseWriter, ID, token string) error {
+	dados := map[string]string{
+		"id":    ID,
+		"token": token,
+	}
+
+	dadosCodificados, erro := s.Encode("dados", dados)
+	if erro != nil {
+		return erro
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "dados",
+		Value:    dadosCodificados,
+		Path:     "/",
+		HttpOnly: true,
+	})
+	return nil
+}
