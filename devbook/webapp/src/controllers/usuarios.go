81eb22189a3dd39e5459b83c76809de58b9abf5f
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-12-30 16:39:39 -0300

    Exclusao de Usuario

--- devbook/webapp/src/controllers/usuarios.go
+++ devbook/webapp/src/controllers/usuarios.go
@@ -130,6 +130,7 @@ func EditarUsuario(w http.ResponseWriter, r *http.Request) {
 	respostas.JSON(w, response.StatusCode, nil)
 }
 
+//Chama a API para atualizar a senha de um usuario
 func AtualizarSenha(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	senhas, erro := json.Marshal(map[string]string{
@@ -146,6 +147,28 @@ func AtualizarSenha(w http.ResponseWriter, r *http.Request) {
 
 	url := fmt.Sprintf("%s/usuarios/%d/atualizar-senha", config.APIURL, usuarioID)
 	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, bytes.NewBuffer(senhas))
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+}
+
+//Chama a API para deletar um usuario
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	url := fmt.Sprintf("%s/usuarios/%d", config.APIURL, usuarioID)
+
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodDelete, url, nil)
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
